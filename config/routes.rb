039d11678d29740098c8a1b275b7e242549e947c
Nea Noc <nea.a.noc@gmail.com>
2023-03-05 21:33:15 -0500

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'pages#about'
+  root to: 'pages#index'
   get 'about', to: 'pages#about'
   get 'gallery', to: 'pages#gallery'
   # Defines the root path route ("/")
